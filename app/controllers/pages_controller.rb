b28557087738e7c05884c64aa9b40cd5dd15ee21
Lika Lex <lika.lex555@gmail.com>
2018-06-01 12:19:56 +0300

    Add will_paginate gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :find_page, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @page = Page.all.order("created_at DESC")
+    @page = Page.all.order("created_at DESC").paginate(page: params[:page], per_page: 6)
   end
 
   def show
