7351b3493e3bb15ba4831090e4ff029a5146dd54
Lika Lex <lika.lex555@gmail.com>
2018-06-01 13:12:41 +0300

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :pages
+  resources :pages do
+    resources :comments
+  end
 
   root "pages#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_160839) do
+ActiveRecord::Schema.define(version: 2018_06_01_093906) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "page_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["page_id"], name: "index_comments_on_page_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
